b1132137fe28e5acb9e3d8798758c5c1499004e5
Jean-Philippe Evrard <jean-philippe@evrard.me>
2018-11-30 17:14:14 +0100

    Add LOCI to OSH images
    This adds the LOCI build progress into OSH-images.
    
    Change-Id: Iffc12af6ef8fff82ca1338c528ea427ef2fb0e8a

---
+++ openstack/loci/build-queens.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SCRIPT=`realpath $0`
+SCRIPT_DIR=`dirname ${SCRIPT}`
+## Only build from main folder
+
+# Replace with Registry URI with your registry like your
+# dockerhub user. Example: "docker.io/openstackhelm"
+VERSION=${VERSION:-"latest"}
+OPENSTACK_VERSION="stable/queens"
+source ${SCRIPT_DIR}/build.sh
