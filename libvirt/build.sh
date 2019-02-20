41b6402415919c7e174dcf9a78120fd33130f912
Jiří Suchomel <jiri.suchomel@suse.com>
2019-02-20 14:46:35 +0100

    Fix typo in argument name
    Change-Id: Ie78addce2e47fc5a799ed287dab200084362af71

--- libvirt/build.sh
+++ libvirt/build.sh
@@ -9,7 +9,7 @@ LIBVIRT_VERSION=${LIBVIRT_VERSION:-"1.3.1-1ubuntu10.24"}
 VERSION=${VERSION:-latest}
 DISTRO=${DISTRO:-ubuntu_xenial}
 REGISTRY_URI=${REGISTRY_URI:-"openstackhelm/"}
-EXTRA_TAG_INFO=${EXTRA_TAB_INFO:-"-${LIBVIRT_VERSION}"}
+EXTRA_TAG_INFO=${EXTRA_TAG_INFO:-"-${LIBVIRT_VERSION}"}
 docker build -f ${IMAGE}/Dockerfile.${DISTRO} --network=host -t ${REGISTRY_URI}${IMAGE}:${VERSION}-${DISTRO}${EXTRA_TAG_INFO} --build-arg LIBVIRT_VERSION="${LIBVIRT_VERSION}" ${extra_build_args} ${IMAGE}
 
 cd -
