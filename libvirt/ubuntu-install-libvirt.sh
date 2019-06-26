03e66fa9a8a55bdbadfa422267f58ca216b27ca5
Pete Birley <pete@port.direct>
2019-06-26 12:51:29 -0500

    Libvirt: Ubuntu Bionic image
    This PS adds a libvirt image based on the bionic release of Ubuntu.
    
    Change-Id: Id87dc3f6a280cf91b3a31a1f847ea1439cbfa797
    Signed-off-by: Pete Birley <pete@port.direct>

---
+++ libvirt/ubuntu-install-libvirt.sh
@@ -0,0 +1,32 @@
+#/bin/bash
+set -ex
+export DEBIAN_FRONTEND=noninteractive ;\
+apt-key add /etc/apt/ceph-${CEPH_RELEASE}.key ;\
+rm -f /etc/apt/ceph-${CEPH_RELEASE}.key ;\
+echo "deb ${CEPH_REPO} ${UBUNTU_RELEASE} main" | tee /etc/apt/sources.list.d/ceph.list ;\
+apt-get update ;\
+apt-get upgrade -y ;\
+apt-get install --no-install-recommends -y \
+  ceph-common \
+  cgroup-tools \
+  dmidecode \
+  ebtables \
+  iproute2 \
+  libvirt-bin \
+  pm-utils \
+  qemu \
+  qemu-block-extra \
+  qemu-efi \
+  openvswitch-switch ;\
+groupadd -g ${GID} ${PROJECT} ;\
+useradd -u ${UID} -g ${PROJECT} -M -d /var/lib/${PROJECT} -s /usr/sbin/nologin -c "${PROJECT} user" ${PROJECT} ;\
+mkdir -p /etc/${PROJECT} /var/log/${PROJECT} /var/lib/${PROJECT} /var/cache/${PROJECT} ;\
+chown ${PROJECT}:${PROJECT} /etc/${PROJECT} /var/log/${PROJECT} /var/lib/${PROJECT} /var/cache/${PROJECT} ;\
+usermod -a -G kvm ${PROJECT} ;\
+apt-get clean -y ;\
+rm -rf \
+   /var/cache/debconf/* \
+   /var/lib/apt/lists/* \
+   /var/log/* \
+   /tmp/* \
+   /var/tmp/*
