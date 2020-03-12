b0746f732b7120e05a15b3e9e8d186d07cb941c5
IPATOV, DENIS (di0361) <di0361@att.com>
2020-03-12 15:09:38 -0700

    Change kubectl binary to kubernetes library
    Change-Id: I9f6ed6f286a50a70380625411b8447709bc64d42

--- nagios/plugins/pvc_pv_rbd_monitoring.py
+++ nagios/plugins/pvc_pv_rbd_monitoring.py
@@ -3,45 +3,20 @@
 import sys
 import subprocess
 import argparse
+import ast
 
 import json
 import logging
 import logging.handlers
 import os
 
+from kubernetes import client
+from kubernetes import config
+from kubernetes.client.rest import ApiException
+from kubernetes.stream import stream
 
-# need to export path to configuration file. should work faster
-if os.environ["KUBECONFIG"] !="":
-    CONFIG = "KUBECONFIG={}".format(os.environ["KUBECONFIG"])
-else:
-    CONFIG = "KUBECONFIG=/etc/kubernetes/admin/kubeconfig.yaml"
-if os.environ["KUBECTL"]  != "":
-    KUBECTL = os.environ["KUBECTL"]
-else:
-    KUBECTL = "/usr/local/bin/kubectl"
-
-OPTIONS = {
-           'read': "-o json",
-           'ns': "-n",
-           'exec': ""
-          }
 
 logger = logging.getLogger(os.path.splitext(os.path.basename(sys.argv[0]))[0])
-
-kubectl = "{config} {kubectl} {options} {namespace}".format(config=CONFIG,
-                                                            kubectl=KUBECTL,
-                                                            namespace=OPTIONS["ns"],
-                                                            options=OPTIONS["read"])
-
-kubectl_exec = "{config} {kubectl} {options} {namespace}".format(config=CONFIG,
-                                                                 kubectl=KUBECTL,
-                                                                 namespace=OPTIONS["ns"],
-                                                                 options=OPTIONS["exec"])
-
-OK       = 0    # 0 - Service is OK
-WARNING  = 1    # 1 - Service has a WARNING.
-CRITICAL = 2    # 2 - Service is in a CRITICAL status.
-UNKNOWN  = 3    # 3 - Service status is UNKNOWN
 
 class CustomFormatter(argparse.RawDescriptionHelpFormatter,
                       argparse.ArgumentDefaultsHelpFormatter):
@@ -64,14 +39,6 @@ def parse_args(args=sys.argv[1:]):
     g.add_argument("--pv", action="store_true",
                    default=False, help="check permanent volumes")
 
-    # configuration options
-    g.add_argument("--bin", "-e", action="store_const",
-                   const=KUBECTL,
-                   help="path to kubecl binary")
-    g.add_argument("--config", "-c", action="store_const",
-                   const=CONFIG,
-                   help="path to kubecl config")
-
     # debug options
     g = parser.add_mutually_exclusive_group()
     g.add_argument("--debug", "-d", action="store_true",
@@ -93,186 +60,138 @@ def setup_logging(options):
         ch.setFormatter(logging.Formatter("%(levelname)s [%(name)s] %(message)s"))
         root.addHandler(ch)
 
+def get_pod_list(api_instance):
+    """ get list of all pods in all namespaces """
+    # Configs can be set in Configuration class directly or using helper utility
+    logger.debug("Listing pods:")
+    try:
+        pod_list = api_instance.list_pod_for_all_namespaces(pretty='false', timeout_seconds=60)
+    except ApiException as e:
+        logger.error("Exception when calling CoreV1Api->list_pods: %s\n" % e)
+    return pod_list.items
 
-def get_podname(label, namespace):
+def get_name_and_namespace(resource_list):
     """ return list of pods in provided namespaces with label """
 
-    logger.debug("Getting pod names with labels {label} in namespcae {namespace}".format(label=label,
-                                                                                         namespace=namespace))
-    command = "{command} {namespace} get pods -l {label}".format(command=kubectl,
-                                                                 namespace=namespace,
-                                                                 label=label)
-    logger.debug(command)
-    try:
-        output = run_command(command)
-    except Exception as e:
-        logger.error("Getting pod names for labels {label} in namespace {namespace}".format(label=label,
-                                                                                            namespace=namespace))
-        sys.exit(1)
-
+    logger.debug("Getting resource names in all namespaces")
     podnames = []
     try:
-        out = json.loads(output)
-        for item in out["items"]:
-            if item['kind'] == "Pod":
-                podnames.append(item['metadata']['name'])
-    except Exception as e:
-        sys.exit(1)
+        for item in resource_list:
+            podnames.append({"name":item.metadata.name, "namespace":item.metadata.namespace})
+    except ApiException as e:
+       print("Exception when calling CoreV1Api->list_node: %s\n" % e)
     return podnames
 
-
-def run_command(command):
-    """ run command in linux shell and return a result """
-
+def exec_command(api_instance, name, namespace, exec_command):
+    """ execute command inside pod """
+    resp = None
     try:
-        result = subprocess.check_output(command,
-                                         shell=True).strip().decode('utf-8')
-    except Exception as e:
-        logger.error(e)
-        sys.exit(1)
-    return result
+        resp = api_instance.read_namespaced_pod(name=name,
+                                                namespace=namespace)
+    except ApiException as e:
+        if e.status != 404:
+            logger.error("Unknown error: %s" % e)
+            exit(1)
 
+    # Calling exec and waiting for response
+    resp = stream(api_instance.connect_get_namespaced_pod_exec,
+                  name,
+                  namespace,
+                  command=exec_command,
+                  stderr=True, stdin=False,
+                  stdout=True, tty=False)
+    return resp
 
-def check_rbd_status(namespace, rbd):
+def get_pvc_list(api_instance):
+    logger.debug("Listing pvc:")
+    ret = api_instance.list_persistent_volume_claim_for_all_namespaces()
+    return ret.items
+
+def get_pv_list(api_instance):
+    logger.debug("Listing pv:")
+    ret = api_instance.list_persistent_volume()
+    return ret.items
+
+def get_list_namespaces(api_instance):
+    # Configs can be set in Configuration class directly or using helper utility
+    ret = api_instance.list_namespace()
+    return ret.items
+
+def get_rbd_list(api_instance):
+    """ return list of rbd in json format """
+    logger.debug("Getting list of rbd")
+
+    ceph_mon = api_instance.list_pod_for_all_namespaces(label_selector="application=ceph,component=mon,release_group=clcp-ucp-ceph-mon", limit=1)
+    ceph_pod = get_name_and_namespace(ceph_mon.items)
+    command = [
+            "rbd",
+            "ls",
+            "--format",
+            "json"]
+    rbd_list_str = exec_command(api_instance, ceph_pod[0]['name'], ceph_pod[0]['namespace'], command)
+    rbd_list = ast.literal_eval(rbd_list_str)
+#    rbd = rbd_list_str[1:-1].split(',')
+#    rbd_list = []
+#    for i in rbd:
+#        rbd_list.append(i[2:-1].replace("'", ''))
+    return rbd_list
+
+def check_rbd_status(api_instance, rbd):
     """ return status or rbd in json format """
 
-    ceph_mon = get_podname("application=ceph,component=mon", namespace)[0]
-    command = "{command} {namespace} exec -it {pod} -- rbd status --format json {rbd}".format(command=kubectl_exec,
-                                                                                              namespace=namespace,
-                                                                                              rbd=rbd, pod=ceph_mon)
-    out = run_command(command)
-    try:
-        output = json.loads(out)
-    except Exception as e:
-        output = "{{'rbd': {}, 'status': {} }}".format(rbd, e)
-    return output
+    ceph_mon = api_instance.list_pod_for_all_namespaces(label_selector="application=ceph,component=mon,release_group=clcp-ucp-ceph-mon", limit=1)
+    ceph_pod = get_name_and_namespace(ceph_mon.items)
+    command = ["rbd", "status", "--format", "json", "{rbd}".format(rbd=rbd)]
+    rbd_status_str = exec_command(api_instance, ceph_pod[0]['name'], ceph_pod[0]['namespace'], command)
+    status = ast.literal_eval(rbd_status_str)
+    rbd_status = json.dumps(status)
+    return rbd_status
 
-
-def get_pvc(namespace):
-    """ return list of permanent volume claim in specific namespace """
-
-    command = "{command} {namespace} get pvc".format(command=kubectl,
-                                                     namespace=namespace)
-    out = run_command(command)
-    try:
-        output = json.loads(out)
-    except Exception as e:
-        # output = {'namespace': namespace, 'status': e}
-        output = []
-    return output
-
-
-def get_pv(persistentvolume, namespace):
-    """ return list of persistent volumes in specific namespace """
-
-    command = "{command} {namespace} get pv {pv}".format(command=kubectl,
-                                                         namespace=namespace,
-                                                         pv=persistentvolume)
-    out = run_command(command)
-    try:
-        output = json.loads(out)
-    except Exception as e:
-        output = {'persistentvolume': namespace, 'status': e}
-    return output
-
-
-def get_namespaces():
-    """ return list of namespaces """
-
-    command = "{command} {namespace} get ns -o json".format(command=kubectl_exec,
-                                                            namespace="ceph")
-    out = run_command(command)
-    try:
-        output = json.loads(out)
-    except Exception as e:
-        output = {"Error": e}
-    return output
-
-
-def get_rbd_list(namespace):
-    """ return list of rbd in json format """
-
-    ceph_mon = get_podname("application=ceph,component=mon", namespace)[0]
-    command = "{command} {namespace} exec -it {pod} -- rbd ls --format json".format(command=kubectl_exec,
-                                                                                    namespace=namespace,
-                                                                                    pod=ceph_mon)
-    out = run_command(command)
-    logger.debug(out)
-    try:
-        output = json.loads(out)
-    except Exception as e:
-        output = "{{'namaspace': {}, 'status': {} }}".format(namespace, e)
-        logger.error("parse json rbd list output: {}".format(e))
-    return output
-
-
-def monitoring_pvc():
-    logger.info("PVCs aren't associated with RBD")
-    rbds = get_rbd_list("ceph")
-    logger.info("Gettign list of namespaces")
-    namespaces = get_namespaces()
-    return_code = OK
-    for namespace in namespaces['items']:
-        ns = namespace['metadata']['name']
-        logger.info("Checking namespace: {}".format(ns))
-        pvc = get_pvc(ns)
-        for pv in pvc['items']:
-            rbd = get_pv(pv['spec']['volumeName'], ns)
-            rbd = rbd['spec']['rbd']['image']
-            if rbd not in rbds:
-                return_code = WARNING
-                print ("pvc_doesnot_have_rbd:{{namespace={},name={}}} 0".format(ns, pv['metadata']['name']))
+def monitoring_pvc(api_instance):
+    logger.debug("PVCs aren't associated with RBD")
+    rbds = get_rbd_list(api_instance)
+    pvs = get_pv_list(api_instance)
+    return_code = 0
+    for pv in pvs:
+        rbd = pv.spec.rbd.image
+        if rbd not in rbds:
+            print ("pvc_doesnot_have_rbd:{{namespace={},name={}}} 0".format(ns, pv['metadata']['name']))
+            return_code = 1
     return return_code
 
-def monitoring_rbd():
-    logger.info("RBD volumes aren't associated with PVC")
-    r = get_rbd_list("ceph")
+def monitoring_rbd(api_instance):
+    logger.debug("RBD volumes aren't associated with PVC")
+    r = get_rbd_list(api_instance)
     rbds = []
-    namespaces = get_namespaces()
-    return_code = OK
-    for namespace in namespaces['items']:
-        ns = namespace['metadata']['name']
-        pvc = get_pvc(ns)
-        for pv in pvc['items']:
-            rbd = get_pv(pv['spec']['volumeName'], ns)
-            rbd = rbd['spec']['rbd']['image']
-            rbds.append(rbd)
-    logger.info(rbd)
+    pvs = get_pv_list(api_instance)
+    return_code = 0
+    for pv in pvs:
+        rbd = pv.spec.rbd.image
+        rbds.append(rbd)
+    logger.debug(rbd)
     for i in r:
         if i not in rbds:
-            return_code = WARNING
-            logger.debug(i)
-            print ("rbd_doesnot_have_pvc:{{name={}}} {}".format(i,  len(check_rbd_status("ceph", i)['watchers'])))
+            logger.debug("rbd {i} not in pv list".format(i=i))
+            print ("rbd_doesnot_have_pvc:{{name={}}} {}".format(i, len(json.loads(check_rbd_status(api_instance,i))['watchers'])))
+            return_code = 1
     return return_code
 
-def monitoring_pv():
-    logger.info("PVs aren't associated with PVC ")
-    command =  "{config} {kubectl} get pv -o json".format(config=CONFIG, kubectl=KUBECTL)
-    logger.debug(command)
-    out = run_command(command)
-    return_code = OK
-    logger.debug(out)
-    try:
-        output = json.loads(out)
-        if len(output['items']) == 0:
-            return_code = OK
-        else:
-            return_code = WARNING
-        for pv in output['items']:
-            if pv['status']['phase'] == "Released":
-                print ("pv_released:{{name={},status={}}} 0".format(pv['metadata']['name'], pv['status']['phase']))
-    except Exception as e:
-        logger.error("gettign rbd list: {}".format(e))
-        return_code = UNKNOWN
+def monitoring_pv(api_instance):
+    logger.debug("PVs aren't associated with PVC ")
+    pvs = get_pv_list(api_instance)
+    return_code = 0
+    for pv in pvs:
+        if pv.status.phase == "Released":
+            print ("pv_released:{{name={},status={}}} 0".format(pv.metadatai.name, pv.status.phase))
+            return_code = 1
     return return_code
 
-def monitoring():
-    return_code = []
-    return_code.append(monitoring_rbd())  
-    return_code.append(monitoring_pvc())
-    return_code.append(monitoring_pv())
-    return max(return_code)
 
+def monitoring(api_instance):
+    return_code = monitoring_rbd(api_instance)
+    return_code = return_code | monitoring_pvc(api_instance)
+    return_code = return_code | monitoring_pv(api_instance)
+    return return_code
 
 def main():
     # nagios monitoring
@@ -280,26 +199,26 @@ def main():
     # 1 - Service has a WARNING.
     # 2 - Service is in a CRITICAL status.
     # 3 - Service status is UNKNOWN
-    return_code = OK
+
     # promotheus text file
     options = parse_args()
     setup_logging(options)
 
+    config.load_kube_config()
+    kube_api = client.CoreV1Api()
 
     if options.all:
-        logger.info("all")
-        return_code = monitoring()
+        logger.debug("all")
+        sys.exit(monitoring(kube_api))
     if options.rbd:
-        logger.info("rbd")
-        return_code = monitoring_rbd()
+        logger.debug("rbd")
+        sys.exit(monitoring_rbd(kube_api))
     if options.pvc:
-        logger.info("pvc")
-        return_code = monitoring_pvc()
+        logger.debug("pvc")
+        sys.exit(monitoring_pvc(kube_api))
     if options.pv:
-        logger.info("pv")
-        return_code = monitoring_pv()
-    sys.exit(return_code)
-
+        logger.debug("pv")
+        sys.exit(monitoring_pv(kube_api))
 
 if __name__ == "__main__":
     main()
