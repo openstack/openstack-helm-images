f1948f99cc29cd7310d282b65ea296e165a8deaa
Pai Radhika <rp592h@att.com>
2020-06-29 22:30:04 +0000

    Node Problem Detector Image
    This ps is to create a new image for NPD in openstack-helm-images
    repo using the Ubuntu base instead of Debian, since the upstream Debian
    image has some security vulnerabilities
    The upstream Docker Image: k8s.gcr.io/node-problem-detector:v0.8.2
    
    Change-Id: I532922a4d678665d184f9eab6fce1a1ce2d1000d

---
+++ node-problem-detector/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SCRIPT=`realpath $0`
+SCRIPT_DIR=`dirname ${SCRIPT}`
+## Only build from main folder
+cd ${SCRIPT_DIR}/..
+
+IMAGE="node-problem-detector"
+VERSION=${VERSION:-latest}
+DISTRO=${DISTRO:-ubuntu_bionic}
+REGISTRY_URI=${REGISTRY_URI:-"openstackhelm/"}
+EXTRA_TAG_INFO=${EXTRA_TAG_INFO:-""}
+
+NPD_VERSION=${NPD_VERSION:-v0.8.2}
+
+docker build -f ${IMAGE}/Dockerfile.${DISTRO} --network=host -t ${REGISTRY_URI}${IMAGE}:${VERSION}-${DISTRO}${EXTRA_TAG_INFO} ${extra_build_args} ${IMAGE}
+
+cd -
