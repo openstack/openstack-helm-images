55a41e9b29f0b47ecde5dddeaa44447f8e44569c
Gage Hugo <gagehugo@gmail.com>
2021-03-24 14:04:29 -0500

    Add ubuntu bionic mariadb file
    This change adds a new mariadb Dockerfile for ubuntu bionic, a
    future change will remove the xenial one.
    
    Change-Id: Id728b13f99702ebced10486d8e15c3eeb3705c2b

--- mariadb/build.sh
+++ mariadb/build.sh
@@ -5,8 +5,8 @@ SCRIPT_DIR=`dirname ${SCRIPT}`
 cd ${SCRIPT_DIR}/..
 
 IMAGE="mariadb"
-VERSION=${VERSION:-latest}
-DISTRO=${DISTRO:-ubuntu_xenial}
+VERSION=${VERSION:-10.5.3-bionic}
+DISTRO=${DISTRO:-ubuntu_bionic}
 REGISTRY_URI=${REGISTRY_URI:-"openstackhelm/"}
 EXTRA_TAG_INFO=${EXTRA_TAG_INFO:-""}
 docker build -f ${IMAGE}/Dockerfile.${DISTRO} --network=host -t ${REGISTRY_URI}${IMAGE}:${VERSION}-${DISTRO}${EXTRA_TAG_INFO} ${extra_build_args} ${IMAGE}
