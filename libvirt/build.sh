d3bf8b7f95918e75784d06b50c2e4c678c01a06b
Gage Hugo <gagehugo@gmail.com>
2022-04-27 12:49:07 -0500

    Remove usage of ubuntu xenial
    This change removes or updates any usage of ubuntu xenial either
    to a newer version or removes any building of xenial images in
    favor of using newer ones.
    
    Change-Id: Ice391a36e17cdbe6783f42aa1d34d7dfe5fb6282

--- libvirt/build.sh
+++ libvirt/build.sh
@@ -7,7 +7,7 @@ cd ${SCRIPT_DIR}/..
 IMAGE="libvirt"
 LIBVIRT_VERSION=${LIBVIRT_VERSION:-"1.3.1-1ubuntu10.24"}
 VERSION=${VERSION:-latest}
-DISTRO=${DISTRO:-ubuntu_xenial}
+DISTRO=${DISTRO:-ubuntu_focal}
 REGISTRY_URI=${REGISTRY_URI:-"openstackhelm/"}
 EXTRA_TAG_INFO=${EXTRA_TAG_INFO:-"-${LIBVIRT_VERSION}"}
 docker build -f ${IMAGE}/Dockerfile.${DISTRO} --network=host -t ${REGISTRY_URI}${IMAGE}:${VERSION}-${DISTRO}${EXTRA_TAG_INFO} --build-arg LIBVIRT_VERSION="${LIBVIRT_VERSION}" ${extra_build_args} ${IMAGE}
