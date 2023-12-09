f381d0474a2fd306e8d09d0fb338b370f10b6d63
Vladimir Kozhukalov <kozhukalov@gmail.com>
2023-12-08 19:47:46 -0600

    Build mariadb jammy images
    Change-Id: I69fa9101a87803e760a1ba61e6d9e86599097359

--- mariadb/build.sh
+++ mariadb/build.sh
@@ -6,9 +6,11 @@ cd ${SCRIPT_DIR}/..
 
 IMAGE="mariadb"
 VERSION=${VERSION:-10.5.9-focal}
-DISTRO=${DISTRO:-ubuntu_focal}
+DISTRO=${DISTRO:-ubuntu}
+DISTRO_VERSION=${DISTRO_VERSION:-focal}
+
 REGISTRY_URI=${REGISTRY_URI:-"openstackhelm/"}
 EXTRA_TAG_INFO=${EXTRA_TAG_INFO:-""}
-docker build -f ${IMAGE}/Dockerfile.${DISTRO} --network=host -t ${REGISTRY_URI}${IMAGE}:${VERSION}-${DISTRO}${EXTRA_TAG_INFO} ${extra_build_args} ${IMAGE}
+docker build -f ${IMAGE}/Dockerfile.${DISTRO} --network=host -t ${REGISTRY_URI}${IMAGE}:${VERSION}-${DISTRO}_${DISTRO_VERSION}${EXTRA_TAG_INFO} ${extra_build_args} ${IMAGE}
 
 cd -
