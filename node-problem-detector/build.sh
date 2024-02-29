9bcdb3c08674fc0cf4f460aa69921c3e417ca760
Alexey Odinokov <aodinokov@mirantis.com>
2024-02-29 15:47:44 -0600

    Update node problem detector image to v0.8.15
    Change-Id: Iee97b968bb0fad03d08f190b692ef7d23d4052d1

--- node-problem-detector/build.sh
+++ node-problem-detector/build.sh
@@ -10,7 +10,7 @@ DISTRO=${DISTRO:-ubuntu_focal}
 REGISTRY_URI=${REGISTRY_URI:-"openstackhelm/"}
 EXTRA_TAG_INFO=${EXTRA_TAG_INFO:-""}
 
-NPD_VERSION=${NPD_VERSION:-v0.8.10}
+NPD_VERSION=${NPD_VERSION:-v0.8.15}
 
 docker build -f ${IMAGE}/Dockerfile.${DISTRO} --network=host -t ${REGISTRY_URI}${IMAGE}:${VERSION}-${DISTRO}${EXTRA_TAG_INFO} ${extra_build_args} ${IMAGE}
 
