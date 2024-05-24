f06375d814938290dc5c37e2f2dcfae8cfade0e6
astebenkova <astebenkova@mirantis.com>
2024-05-24 18:14:44 +0300

    [nagios] Build Jammy-based image
    + unify Dockerfiles for different Ubuntu versions
    + switch plugins to use Python 3, as it is the only supported version starting from Ubuntu Focal
    
    Change-Id: I0e66ed0c96ed72c8202940e8a97db17d932a5e47

--- nagios/build.sh
+++ nagios/build.sh
@@ -6,9 +6,10 @@ cd ${SCRIPT_DIR}/..
 
 IMAGE="nagios"
 VERSION=${VERSION:-latest}
-DISTRO=${DISTRO:-ubuntu_focal}
+DISTRO=${DISTRO:-ubuntu}
+DISTRO_VERSION=${DISTRO_VERSION:-focal}
 REGISTRY_URI=${REGISTRY_URI:-"openstackhelm/"}
 EXTRA_TAG_INFO=${EXTRA_TAG_INFO:-""}
-docker build -f ${IMAGE}/Dockerfile.${DISTRO} --network=host -t ${REGISTRY_URI}${IMAGE}:${VERSION}-${DISTRO}${EXTRA_TAG_INFO} ${extra_build_args} ${IMAGE}
+docker build -f ${IMAGE}/Dockerfile.${DISTRO} --network=host --build-arg="FROM=${DISTRO}:${DISTRO_VERSION}" -t ${REGISTRY_URI}${IMAGE}:${VERSION}-${DISTRO}${EXTRA_TAG_INFO} ${extra_build_args} ${IMAGE}
 
 cd -
