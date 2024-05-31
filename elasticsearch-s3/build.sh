59de48379bd7162338d5e94ab3ee794424ad3a32
astebenkova <astebenkova@mirantis.com>
2024-05-31 17:05:09 +0300

    [elasticsearch-s3] Prepare recipes for v7/v8
    Create common Dockerfiles for the latest major ES versions
    
    Change-Id: I5b1232e41e6713cdf89b832262ec5a703cfaf81d

--- elasticsearch-s3/build.sh
+++ elasticsearch-s3/build.sh
@@ -5,10 +5,10 @@ SCRIPT_DIR=`dirname ${SCRIPT}`
 cd ${SCRIPT_DIR}/..
 
 IMAGE="elasticsearch-s3"
-VERSION=${VERSION:-latest}
-DISTRO=${DISTRO:-centos_7}
+VERSION=${VERSION:-8.13.4}
+MAJOR_VERSION=${MAJOR_VERSION:-8}
 REGISTRY_URI=${REGISTRY_URI:-"openstackhelm/"}
 EXTRA_TAG_INFO=${EXTRA_TAG_INFO:-""}
-docker build -f ${IMAGE}/Dockerfile.${DISTRO} --network=host -t ${REGISTRY_URI}${IMAGE}:${VERSION}-${DISTRO}${EXTRA_TAG_INFO} ${extra_build_args} ${IMAGE}
+docker build -f ${IMAGE}/Dockerfile.${MAJOR_VERSION} --network=host --build-arg="ELASTICSEARCH_VERSION=${VERSION}" -t ${REGISTRY_URI}${IMAGE}:${VERSION}${EXTRA_TAG_INFO} ${extra_build_args} ${IMAGE}
 
 cd -
