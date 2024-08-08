019613c27f9dd4e156cc95b40ac20148ff64d775
astebenkova <astebenkova@mirantis.com>
2024-08-08 15:39:00 +0300

    [ceph] Refactor images
    + create a single Dockerfile for different Ubuntu versions
    + pin setuptools version to v70.3.0 due to compilation issues
    
    Change-Id: I4b3a8f9554461390e355a4d9ddb945cb4e6035d7

--- ceph-utility/build.sh
+++ ceph-utility/build.sh
@@ -6,9 +6,14 @@ cd ${SCRIPT_DIR}/..
 
 IMAGE="ceph-utility"
 VERSION=${VERSION:-latest}
-DISTRO=${DISTRO:-ubuntu_jammy}
+DISTRO=${DISTRO:-ubuntu}
+DISTRO_VERSION=${DISTRO_VERSION:-focal}
 REGISTRY_URI=${REGISTRY_URI:-"openstackhelm/"}
 EXTRA_TAG_INFO=${EXTRA_TAG_INFO:-""}
-docker build -f ${IMAGE}/Dockerfile.${DISTRO} --network=host -t ${REGISTRY_URI}${IMAGE}:${VERSION}-${DISTRO}${EXTRA_TAG_INFO} ${extra_build_args} ${IMAGE}
+docker build --file=${IMAGE}/Dockerfile.${DISTRO} \
+             --network=host \
+             --build-arg="FROM=${DISTRO}:${DISTRO_VERSION}" \
+             --tag=${REGISTRY_URI}${IMAGE}:${VERSION}-${DISTRO}_${DISTRO_VERSION}${EXTRA_TAG_INFO} \
+             ${extra_build_args} ${IMAGE}
 
 cd -
